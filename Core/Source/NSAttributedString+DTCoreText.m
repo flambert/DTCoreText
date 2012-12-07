5262c80475a0c6874faa0ccf590d8f7f6abc375a
Francois Lambert <flambert@mirego.com>
2012-12-07 14:57:05 -0500

    Fix warning

--- Core/Source/NSAttributedString+DTCoreText.m
+++ Core/Source/NSAttributedString+DTCoreText.m
@@ -14,6 +14,7 @@
 #import "DTCoreTextFontDescriptor.h"
 #import "NSString+HTML.h"
 #import "DTColor+HTML.h"
+#import "NSAttributedStringRunDelegates.h"
 
 @implementation NSAttributedString (DTCoreText)
 
